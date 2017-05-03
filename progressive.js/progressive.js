624aa57660e3c493e5ba04f4ccbaf454db00abbc
robin <lb.robin1991@gmail.com>
2017-05-03 18:11:21 +0800

    update demo & fix bugs

---
+++ progressive.js/progressive.js
@@ -0,0 +1,94 @@
+;(function(window, undefined){
+  function progressive(opts){
+    var $el = document.querySelectorAll(opts.el);
+
+    function support_canvas(){
+      var elem = document.createElement('canvas');
+      return !!(elem.getContext && elem.getContext('2d'));
+    }
+
+    if (!support_canvas()){
+      // 不支持canvas，直接加载原图
+      $el.forEach(function(el, i){
+        var src = el.getAttribute('data-src');
+        el.style.backgroundImage = 'url('+ src +')'
+      });
+      return false;
+    }
+
+    var defaults = {
+      // el: '.header',
+      // canvasWidth: auto,
+      // canvasHeight: '540'
+      blur: '10px',
+      // zIndex: -1,
+      useElOffset: true
+    };
+    for (var i in defaults){
+      if (defaults.hasOwnProperty(i)){
+        opts[i] = (opts[i] !== undefined ? opts[i] : defaults[i]);
+      }
+    }
+
+    $el.forEach(function(el, i){
+      var elH = el.offsetHeight;
+      var elW = el.offsetWidth;
+      var canvas = document.createElement('canvas');
+      var ctx = canvas.getContext("2d");
+      var imageThumb = new Image();
+
+      canvas.className = 'canvas';
+      canvas.width = opts.canvasWidth || elW;
+      canvas.height = opts.canvasHeight || elH;
+
+      if (!opts.useElOffset){
+        opts.canvasWidth && (canvas.style.width = opts.canvasWidth + 'px');
+        opts.canvasHeight && (canvas.style.height = opts.canvasHeight + 'px');
+      }
+
+      opts.zIndex && (canvas.zIndex = opts.zIndex);
+      ctx.filter = 'blur(' + opts.blur + ')';
+      el.insertBefore(canvas, el.firstChild);
+
+
+      // 缩略图
+      imageThumb.onload = function(){
+        var sx = 0;
+        var sy = 0;
+        var swidth = imageThumb.width;
+        var sheight = imageThumb.height;
+
+        var rate = elH/sheight; // 以高度为缩放标准
+        var width = imageThumb.width*rate;
+        var height = imageThumb.height*rate;
+
+        //居中显示
+        var x = (elW - width) / 2;   // 使用 canvas 的宽度减去图片宽度, 计算居中位置
+        var y = (elH - height) / 2; // 使用 canvas 的高度减去图片高度, 计算居中位置
+        ctx.drawImage(imageThumb, sx, sy, swidth, sheight, x, y, width, height);
+        imageThumb = null;
+
+        loadOrigin(el, canvas);
+      };
+      imageThumb.src = el.getAttribute('data-thumb');
+
+    });
+
+
+    function loadOrigin(el, canvas){
+      // 加载原图
+      var imageOrigin = new Image();
+      imageOrigin.onload = function(){
+        el.style.backgroundImage = 'url('+ imageOrigin.src +')';
+        canvas.className = canvas.className + ' canvas--hidden';
+        // ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
+        imageOrigin = canvas = null;
+        // console.log('loaded');
+      };
+      imageOrigin.src = el.getAttribute('data-src');
+    }
+  }
+
+  window.progressive = progressive;
+
+})(window)
