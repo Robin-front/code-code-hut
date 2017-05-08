23cbdf2ec78e46b30e7e8d56668f30df1b88bab5
robin <lb.robin1991@gmail.com>
2017-05-08 13:58:09 +0800

    wrap UMD

--- progressive.js/progressive.js
+++ progressive.js/progressive.js
@@ -1,17 +1,27 @@
-;(function(window, undefined){
-  function progressive(opts){
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.progressive = factory();
+  }
+}(this, function() {
+  'use strict';
+
+  function progressive(opts) {
     var $el = document.querySelectorAll(opts.el);
 
-    function support_canvas(){
+    function support_canvas() {
       var elem = document.createElement('canvas');
       return !!(elem.getContext && elem.getContext('2d'));
     }
 
-    if (!support_canvas()){
+    if (!support_canvas()) {
       // 不支持canvas，直接加载原图
-      $el.forEach(function(el, i){
+      $el.forEach(function(el, i) {
         var src = el.getAttribute('data-src');
-        el.style.backgroundImage = 'url('+ src +')'
+        el.style.backgroundImage = 'url(' + src + ')'
       });
       return false;
     }
@@ -25,13 +35,13 @@
       // zIndex: -1,
       useElOffset: true
     };
-    for (var i in defaults){
-      if (defaults.hasOwnProperty(i)){
+    for (var i in defaults) {
+      if (defaults.hasOwnProperty(i)) {
         opts[i] = (opts[i] !== undefined ? opts[i] : defaults[i]);
       }
     }
 
-    $el.forEach(function(el, i){
+    $el.forEach(function(el, i) {
       var elH = el.offsetHeight;
       var elW = el.offsetWidth;
       var canvas = document.createElement('canvas');
@@ -42,7 +52,7 @@
       canvas.width = opts.canvasWidth || elW;
       canvas.height = opts.canvasHeight || elH;
 
-      if (!opts.useElOffset){
+      if (!opts.useElOffset) {
         opts.canvasWidth && (canvas.style.width = opts.canvasWidth + 'px');
         opts.canvasHeight && (canvas.style.height = opts.canvasHeight + 'px');
       }
@@ -53,17 +63,17 @@
 
 
       // 缩略图
-      imageThumb.onload = function(){
+      imageThumb.onload = function() {
         var sx = 0;
         var sy = 0;
         var swidth = imageThumb.width;
         var sheight = imageThumb.height;
 
-        var rate = opts.zoom === 'height' ? elH/sheight : elW/swidth; // 以高度为缩放标准
-        var width = imageThumb.width*rate;
-        var height = imageThumb.height*rate;
+        var rate = opts.zoom === 'height' ? elH / sheight : elW / swidth; // 以高度为缩放标准
+        var width = imageThumb.width * rate;
+        var height = imageThumb.height * rate;
         //居中显示
-        var x = (elW - width) / 2;   // 使用 canvas 的宽度减去图片宽度, 计算居中位置
+        var x = (elW - width) / 2; // 使用 canvas 的宽度减去图片宽度, 计算居中位置
         var y = (elH - height) / 2; // 使用 canvas 的高度减去图片高度, 计算居中位置
         ctx.drawImage(imageThumb, sx, sy, swidth, sheight, x, y, width, height);
         imageThumb = null;
@@ -75,11 +85,11 @@
     });
 
 
-    function loadOrigin(el, canvas){
+    function loadOrigin(el, canvas) {
       // 加载原图
       var imageOrigin = new Image();
-      imageOrigin.onload = function(){
-        el.style.backgroundImage = 'url('+ imageOrigin.src +')';
+      imageOrigin.onload = function() {
+        el.style.backgroundImage = 'url(' + imageOrigin.src + ')';
         canvas.className = canvas.className + ' canvas--hidden';
         // ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
         imageOrigin = canvas = null;
@@ -88,6 +98,12 @@
       imageOrigin.src = el.getAttribute('data-src');
     }
   }
+  return progressive;
+}));
+
+;
+(function(window, undefined) {
+
 
   window.progressive = progressive;
 
