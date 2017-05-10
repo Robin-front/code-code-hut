226f4056365bfad72e7d25f4ba7292b4ad976fba
robin <lb.robin1991@gmail.com>
2017-05-10 18:15:42 +0800

    rodash

---
+++ rodash/rodash.js
@@ -0,0 +1,21 @@
+
+var _ = {};
+
+function createround(operator){
+  var func = Math[operator];
+  return function(number, precision){
+    number = Number(number);
+    precision = precision == null ? 0 : Number(precision);
+    if (precision){
+      var pair, value = func(number * Math.pow(10, precision));
+      return value * Math.pow(10, -precision);
+    }
+
+    return func(number);
+  }
+}
+
+
+// _.ceil(6.004, 2) => 6.01
+// _ceil(6040, -2) => 6100
+_.ceil = createround('ceil');
