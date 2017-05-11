4ed1b0897c9795d2b7c15c2d075f9f3447360268
robin <lb.robin1991@gmail.com>
2017-05-11 09:28:53 +0800

    floor & round

--- rodash/rodash.js
+++ rodash/rodash.js
@@ -15,7 +15,36 @@ function createround(operator){
   }
 }
 
-
+// _ceil(6.004) => 7
 // _.ceil(6.004, 2) => 6.01
 // _ceil(6040, -2) => 6100
-_.ceil = createround('ceil');
+var ceil = createround('ceil');
+
+/**
+ *
+ * _.floor(4.006);
+ * // => 4
+ *
+ * _.floor(0.046, 2);
+ * // => 0.04
+ *
+ * _.floor(4060, -2);
+ * // => 4000
+ **/
+var floor = createround('floor')
+
+/**
+  * _.round(4.006);
+  * // => 4
+  *
+  * _.round(4.006, 2);
+  * // => 4.01
+  *
+  * _.round(4060, -2);
+  * // => 4100
+ */
+var round = createround('round');
+
+_.ceil = ceil;
+_.floor = floor;
+_.round = round;
