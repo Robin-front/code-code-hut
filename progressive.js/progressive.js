7d44adc84993560c9872444c1d9d167283e996d3
robin <lb.robin1991@gmail.com>
2017-05-15 10:28:54 +0800

    注释

--- progressive.js/progressive.js
+++ progressive.js/progressive.js
@@ -149,6 +149,16 @@
       window.addEventListener(evt, lazy, false);
     });
 
+    // trigger event
+    // var event;
+    // if (document.createEvent) {
+    //     event = document.createEvent("HTMLEvents");
+    //     event.initEvent("scroll", true, true);
+    //   } else {
+    //     event = document.createEventObject();
+    //     event.eventType = "scroll";
+    //   }
+    // window.dispatchEvent(event);
 
   }
   return progressive;
