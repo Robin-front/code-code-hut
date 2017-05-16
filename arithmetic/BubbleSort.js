a8b75d9bfb418cdd509cf2a1510cb3e35045f068
robin <lb.robin1991@gmail.com>
2017-05-16 14:27:41 +0800

    bubble sort

---
+++ arithmetic/BubbleSort.js
@@ -0,0 +1,17 @@
+// 每次找出最大值或最小值移到最右边
+
+var bubbleSort = function (arr){
+  var i, j, count = arr.length -1, temp;
+  for (i = 0;i < count; i++){
+    // i可以理解为已排好的个数
+    for (j = count; j > i; j--){
+      // 从后往前冒泡，每次找出最小的数
+      if (arr[j] < arr[j-1]){
+        temp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = temp;
+      }
+    }
+  }
+  return arr;
+}
